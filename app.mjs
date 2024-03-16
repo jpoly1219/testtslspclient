57dffc2a67b61b40518f336cb600d98a25b50a68
Jacob Kim <jpoly1219@gmail.com>
2024-03-16 00:08:22 -0400

    handle cli arguments

--- app.mjs
+++ app.mjs
@@ -4,6 +4,26 @@ import * as fs from "fs"
 import { indexOfRegexGroup } from "./utils.mjs";
 import { recursiveDefine } from "./core.mjs";
 
+// expected arguments: directory to run the type extraction
+const outputfile = fs.createWriteStream("log.txt")
+let rootUri = "";
+let workspaceFolders = [];
+let sketchFile = "";
+if (process.argv.length < 3) {
+  console.error("error: target directory and code sketch file not specified");
+  process.exit(1);
+} else if (process.argv.length < 4) {
+  console.error("error: missing either the target directory or code sketch file");
+  process.exit(1);
+} else if (process.argv.length > 4) {
+  console.error("error: too many arguments given");
+  process.exit(1);
+} else {
+  rootUri = process.argv[2];
+  workspaceFolders = [{ 'name': 'testtslspclient', 'uri': rootUri }];
+  sketchFile = process.argv[3];
+}
+
 const r = spawn('typescript-language-server', ['--stdio'])
 const e = new JSONRPCEndpoint(r.stdin, r.stdout)
 const c = new LspClient(e)
@@ -151,9 +171,8 @@ const capabilities = {
     'positionEncodings': ['utf-8']
   },
 }
-const root_uri = 'file:///home/jacob/projects/testtslspclient/'
-const workspaceFolders = [{ 'name': 'testtslspclient', 'uri': root_uri }]
-const outputfile = fs.createWriteStream("log.txt")
+
+
 r.stdout.on('data', (d) => outputfile.write(d))
 
 const resInit = await c.initialize({
@@ -171,122 +190,5 @@ const resInit = await c.initialize({
 });
 
 
-// Getting context of the hole
-
-// method 1: match for common patterns
-// from corpus, find the first instance of __HOLE__
-// loop backwards to see if we can regex match a pattern
-
-
-console.log("\n=== Testing Scattered Type Definitions ===\n")
-
-const notifOpenDefAC = c.didOpen({
-  textDocument: {
-    uri: 'file:///home/jacob/projects/testtslspclient/def_ac.ts',
-    languageId: 'ts',
-    text: 'import { Banana } from "./def_b";\n/* FILE 2: */\n/* code ... */\ntype Apple = {\n  name: string;\n  b: Banana;\n}\n/* code ... */\ntype Cherry = boolean;\n/* code ... */\nexport { Apple, Cherry };',
-    version: 1
-  }
-});
-
-const notifOpenDefB = await c.didOpen({
-  textDocument: {
-    uri: 'file:///home/jacob/projects/testtslspclient/def_b.ts',
-    languageId: 'ts',
-    text: '/* FILE 1: *a\n/* code ... */\ntype Banana = number;\n/* code ... */\nexport { Banana };',
-    version: 1
-  }
-});
-
-const notifOpenTestTogether = await c.didOpen({
-  textDocument: {
-    uri: 'file:///home/jacob/projects/testtslspclient/test_together.ts',
-    languageId: 'ts',
-    text: 'import { Apple, Cherry } from "./def_ac";\n/* FILE 3: */\n/* code ... */\nconst myFunc: (_: Apple) => Cherry =\n  __HOLE__;\n/* code ... */',
-    version: 1
-  }
-});
-console.log(notifOpenDefAC, notifOpenDefB, notifOpenTestTogether)
-
-const ttcorpus = 'import { Apple, Cherry } from "./def_ac";\n/* FILE 3: */\n/* code ... */\nconst myFunc: (_: Apple) => Cherry =\n  __HOLE__;\n/* code ... */';
-// pattern 1: const myFunc: (_: T1) => T2 =\n  __HOLE__
-// divided into groups: const .+: \(.+: .+\) => .+ =\n __HOLE__
-//                      1^^^^^2^3^^^4^5^6^7^^^^^8^9^^^^^^^^^^^^
-const ttpattern = /(const )(.+)(: \()(.+)(: )(.+)(\) => )(.+)( =\n  __HOLE__)/;
-
-const ttfirstPatternIndex = ttcorpus.search(ttpattern);
-const ttmatch = ttcorpus.match(pattern);
-const ttmatchedFunctionName = ttmatch[2];
-
-let ttfromBeginning = ttcorpus.substring(0, ttfirstPatternIndex);
-let ttlineNumber = (ttfromBeginning.match(/\n/g)).length;
-
-// type of function
-const ttresHoverFunctionTypeMatch = await c.hover({
-  textDocument: {
-    uri: 'file:///home/jacob/projects/testtslspclient/test_together.ts'
-  },
-  position: {
-    character: indexOfRegexGroup(ttmatch, 2) - ttfirstPatternIndex,
-    line: ttlineNumber
-  }
-});
-
-const ttfunctionTypeSignature = ttresHoverFunctionTypeMatch.contents.value.split("\n").reduce((acc, curr) => {
-  if (curr != "" && curr != "```typescript" && curr != "```") {
-    return acc + curr;
-  } else {
-    return acc;
-  }
-}, "");
-
-console.log(`function ${ttmatchedFunctionName}'s type signature: ${ttfunctionTypeSignature}`);
-
-const ttresHoverArgumentTypeMatch = await c.hover({
-  textDocument: {
-    uri: 'file:///home/jacob/projects/testtslspclient/test_together.ts'
-  },
-  position: {
-    character: indexOfRegexGroup(ttmatch, 6) - ttfirstPatternIndex,
-    line: ttlineNumber
-  }
-});
-console.log(ttresHoverArgumentTypeMatch)
-
-const ttargumentTypeSignature = ttresHoverArgumentTypeMatch.contents.value.split("\n").reduce((acc, curr) => {
-  if (curr != "" && curr != "```typescript" && curr != "```") {
-    return acc + curr;
-  } else {
-    return acc;
-  }
-}, "");
-console.log(`argument's type signature: ${ttargumentTypeSignature}`);
-
-
-const ttresTypeDefinitionArgumentTypeMatch = await c.typeDefinition({
-  textDocument: {
-    uri: 'file:///home/jacob/projects/testtslspclient/test_together.ts'
-  },
-  position: {
-    character: indexOfRegexGroup(ttmatch, 6) - ttfirstPatternIndex,
-    line: ttlineNumber
-  }
-});
-console.log(JSON.stringify(ttresTypeDefinitionArgumentTypeMatch, "", 2))
-
-
-const ttresDefinitionArgumentTypeMatch = await c.definition({
-  textDocument: {
-    uri: 'file:///home/jacob/projects/testtslspclient/test_together.ts'
-  },
-  position: {
-    character: indexOfRegexGroup(ttmatch, 6) - ttfirstPatternIndex,
-    line: ttlineNumber
-  }
-});
-console.log(JSON.stringify(ttresDefinitionArgumentTypeMatch, "", 2))
-
-console.log("start with: ", ttfunctionTypeSignature, ttlineNumber, indexOfRegexGroup(ttmatch, 3) + 2 - ttfirstPatternIndex);
-const ttfoundSoFar = new Map();
-await recursiveDefine(c, ttfunctionTypeSignature, ttlineNumber, indexOfRegexGroup(ttmatch, 3) + 2 - ttfirstPatternIndex, ttfoundSoFar, 'file:///home/jacob/projects/testtslspclient/test_together.ts');
-console.log(ttfoundSoFar);
+// get context of the hole
+// recursively define relevant types
