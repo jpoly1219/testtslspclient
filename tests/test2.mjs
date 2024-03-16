1535d495f69b38004d2ae96dbcb5916b607c628d
Jacob Kim <jpoly1219@gmail.com>
2024-03-15 23:43:02 -0400

    create separate client instances for each test

--- tests/test2.mjs
+++ tests/test2.mjs
@@ -1,4 +1,174 @@
-import { c } from "../app.mjs"
+import { JSONRPCEndpoint, LspClient } from "../../ts-lsp-client/build/src/main.js"
+import { spawn } from "child_process";
+import * as fs from "fs"
+import { indexOfRegexGroup } from "../utils.mjs";
+import { recursiveDefine } from "../core.mjs";
+
+const r = spawn('typescript-language-server', ['--stdio'])
+const e = new JSONRPCEndpoint(r.stdin, r.stdout)
+const c = new LspClient(e)
+
+const capabilities = {
+  'textDocument': {
+    'codeAction': { 'dynamicRegistration': true },
+    'codeLens': { 'dynamicRegistration': true },
+    'colorProvider': { 'dynamicRegistration': true },
+    'completion': {
+      'completionItem': {
+        'commitCharactersSupport': true,
+        'documentationFormat': ['markdown', 'plaintext'],
+        'snippetSupport': true
+      },
+      'completionItemKind': {
+        'valueSet': [1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+          10,
+          11,
+          12,
+          13,
+          14,
+          15,
+          16,
+          17,
+          18,
+          19,
+          20,
+          21,
+          22,
+          23,
+          24,
+          25]
+      },
+      'contextSupport': true,
+      'dynamicRegistration': true
+    },
+    'definition': { 'dynamicRegistration': true },
+    'documentHighlight': { 'dynamicRegistration': true },
+    'documentLink': { 'dynamicRegistration': true },
+    'documentSymbol': {
+      'dynamicRegistration': true,
+      'symbolKind': {
+        'valueSet': [1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+          10,
+          11,
+          12,
+          13,
+          14,
+          15,
+          16,
+          17,
+          18,
+          19,
+          20,
+          21,
+          22,
+          23,
+          24,
+          25,
+          26]
+      }
+    },
+    'formatting': { 'dynamicRegistration': true },
+    'hover': {
+      'contentFormat': ['markdown', 'plaintext'],
+      'dynamicRegistration': true
+    },
+    'implementation': { 'dynamicRegistration': true },
+    'inlayhint': { 'dynamicRegistration': true },
+    'onTypeFormatting': { 'dynamicRegistration': true },
+    'publishDiagnostics': { 'relatedInformation': true },
+    'rangeFormatting': { 'dynamicRegistration': true },
+    'references': { 'dynamicRegistration': true },
+    'rename': { 'dynamicRegistration': true },
+    'signatureHelp': {
+      'dynamicRegistration': true,
+      'signatureInformation': { 'documentationFormat': ['markdown', 'plaintext'] }
+    },
+    'synchronization': {
+      'didSave': true,
+      'dynamicRegistration': true,
+      'willSave': true,
+      'willSaveWaitUntil': true
+    },
+    'typeDefinition': { 'dynamicRegistration': true, 'linkSupport': true },
+    'typeHierarchy': { 'dynamicRegistration': true }
+  },
+  'workspace': {
+    'applyEdit': true,
+    'configuration': true,
+    'didChangeConfiguration': { 'dynamicRegistration': true },
+    'didChangeWatchedFiles': { 'dynamicRegistration': true },
+    'executeCommand': { 'dynamicRegistration': true },
+    'symbol': {
+      'dynamicRegistration': true,
+      'symbolKind': {
+        'valueSet': [1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+          10,
+          11,
+          12,
+          13,
+          14,
+          15,
+          16,
+          17,
+          18,
+          19,
+          20,
+          21,
+          22,
+          23,
+          24,
+          25,
+          26]
+      }
+    }, 'workspaceEdit': { 'documentChanges': true },
+    'workspaceFolders': true
+  },
+  'general': {
+    'positionEncodings': ['utf-8']
+  },
+}
+const root_uri = 'file:///home/jacob/projects/testtslspclient/'
+const workspaceFolders = [{ 'name': 'testtslspclient', 'uri': root_uri }]
+const outputfile = fs.createWriteStream("log.txt")
+r.stdout.on('data', (d) => outputfile.write(d))
+
+const resInit = await c.initialize({
+  processId: process.pid,
+  // rootPath: '.',
+  // rootUri: null,
+  capabilities: capabilities,
+  trace: 'off',
+  workspaceFolders: workspaceFolders,
+  initializationOptions: {
+    preferences: {
+      includeInlayVariableTypeHints: true
+    }
+  }
+});
 console.log("=========Test 2==========");
 
 const corpus = 'type T2 = number;\n\ntype T1 = {\n  name: string;\n  t2: T2;\n}\n\ntype T3 = boolean;\n\nconst myFunc: (_: T1) => T3 =\n  __HOLE__;\n\n/* should return \n\ntype B = number;\ntype A = {\n  name: string;\n  b: B;\n}\ntype C = boolean;\n\n*/';
@@ -138,3 +308,8 @@ const returnTypeSignature = resHoverReturnTypeMatch.contents.value.split("\n").r
   }
 }, "");
 console.log(`return's type signature: ${returnTypeSignature}`);
+
+console.log("start with: ", functionTypeSignature, lineNumber, indexOfRegexGroup(match, 3) + 2 - firstPatternIndex);
+const foundSoFar = new Map();
+await recursiveDefine(c, functionTypeSignature, lineNumber, indexOfRegexGroup(match, 3) + 2 - firstPatternIndex, foundSoFar, 'file:///home/jacob/projects/testtslspclient/test2.ts');
+console.log(foundSoFar);
