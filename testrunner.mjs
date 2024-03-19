eef006fa4eb9602177c0875dba1d962f4fe708b3
Jacob Kim <jpoly1219@gmail.com>
2024-03-19 01:41:50 -0400

    init testrunner

---
+++ testrunner.mjs
@@ -0,0 +1,7 @@
+// testrunner
+// accept test directory with a code sketch in it
+// call node app.mjs <dir> <sketch>
+// get the returned types
+// ask LLM to complete the sketch using the returned types
+// write completion to sketch
+// run the prelude - completed sketch - epilogue suite on specific tests
