7bddc467c1e67ce095658589b3db5426dfdd6081
Jacob Kim <jpoly1219@gmail.com>
2024-03-19 01:59:22 -0400

    wrote preliminary code

--- testrunner.mjs
+++ testrunner.mjs
@@ -1,7 +1,38 @@
+import { execSync } from "child_process";
+import * as fs from "fs";
 // testrunner
-// accept test directory with a code sketch in it
-// call node app.mjs <dir> <sketch>
+// cli args:
+// runName
+// errorRoundsMax
+// sourceFolder (with trailing slash)
+// apiKey
+// (expectedType) will be supplied by running app.mjs
+const runName = process.argv[2];
+const errorRoundsMax = process.argv[3];
+const sourceFolder = process.argv[4];
+const apiKey = process.argv[5];
+
+// call node app.mjs <dir> sketch.ts
+execSync(`node app.mjs ${sourceFolder} sketch.ts`)
+
 // get the returned types
+const expectedType = fs.readFileSync("output.txt", "utf8");
+
 // ask LLM to complete the sketch using the returned types
+
+// instantiate connection and save conn object
+
+const sketchFilePath = `${sourceFolder}sketch.ts`;
+const sketchFileContent = fs.readFileSync(sketchFilePath, "utf8");
+const prompt =
+  `Complete the following typescript program sketch with a hole in it.\n
+  Here is the program sketch: \n ${sketchFileContent} \n
+  Here are the relevant contexts: \n ${expectedType} \n`
+
+const response = None
+const completedSketch = None
 // write completion to sketch
+const outputFile = fs.createWriteStream("completed_sketch.ts");
+outputFile.write(completedSketch);
+
 // run the prelude - completed sketch - epilogue suite on specific tests
