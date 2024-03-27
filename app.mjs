5f495ae3243625058eb2204e055224f4c4f6f93c
Jacob Kim <jpoly1219@gmail.com>
2024-03-27 03:45:42 -0400

    foundSoFar is a set instead of a map

--- app.mjs
+++ app.mjs
@@ -234,7 +234,7 @@ c.didChange({
 
 // recursively define relevant types
 const outputFile = fs.createWriteStream("output.txt");
-const foundSoFar = new Map();
+const foundSoFar = new Set();
 await extractRelevantTypes(c, holeContext.functionName, holeContext.functionTypeSpan, 0, "declare function _(): ".length, foundSoFar, injectedSketchFilePath, outputFile);
 console.log(foundSoFar);
 
