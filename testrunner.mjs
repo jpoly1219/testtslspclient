0648f3f13b6b2d50828a61c284a8815618a7fcb8
Jacob Kim <jpoly1219@gmail.com>
2024-03-28 01:41:41 -0400

    test tsc error outputs

--- testrunner.mjs
+++ testrunner.mjs
@@ -12,6 +12,8 @@ const errorRoundsMax = process.argv[3];
 const sourceFolder = process.argv[4];
 const apiKey = process.argv[5];
 
+console.log(runName, errorRoundsMax, sourceFolder, apiKey)
+
 // inject hole function into sketch.ts
 const sketchFilePath = `${sourceFolder}sketch.ts`;
 const sketchFileContent = fs.readFileSync(sketchFilePath, "utf8");
@@ -37,11 +39,13 @@ const prompt =
   Here is the program sketch: \n ${sketchFileContent} \n
   Here are the relevant contexts: \n ${expectedType} \n`;
 
-const response = None;
-const completedSketch = None;
+const response = null;
+const completedSketch = "console.log('hello world!')";
+const erroneousSketch = "console.log(hello world!)";
 // write completion to sketch
-const outputFile = fs.createWriteStream(`${sourceFolder}/completed_sketch.ts`);
-outputFile.write(completedSketch);
+fs.writeFileSync(`${sourceFolder}completed_sketch.ts`, completedSketch);
+fs.writeFileSync(`${sourceFolder}erroneous_sketch.ts`, erroneousSketch);
 
 // run the prelude - completed sketch - epilogue suite on specific tests
-execSync(`node ${sourceFolder}/completed_sketch.ts`);
+execSync(`node ${sourceFolder}completed_sketch.ts`, { stdio: "inherit" });
+execSync(`tsc ${sourceFolder}erroneous_sketch.ts`, { stdio: "inherit" });
